0588201d3d8930df49e69ef6389e2d5a53f2f929
Yury Nevalenny <unevalenniy@smartru.com>
2016-11-12 21:13:12 +0300

    Xamarin.Forms solution

---
+++ Xamarin.Forms/xamarinMeetup2/ItemModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+
+namespace xamarinMeetup2
+{
+    public class ItemModel
+    {
+        //public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        //public string email { get; set; }
+        public string picture { get; set; }
+
+        public string Text
+        {
+            get
+            {
+                return first_name + " " + last_name;
+            }
+        }
+
+        public ImageSource Picture
+        {
+            get
+            {
+                // we remove "data:image/png;base64," from each string start and convert raw base64
+                Byte[] ImageFotoBase64 = System.Convert.FromBase64String(picture.Remove(0, 22));
+                return ImageSource.FromStream(() => new MemoryStream(ImageFotoBase64)); ;
+            }
+        }
+    }
+}
---
+++ Xamarin.Forms/xamarinMeetup2/xamarinMeetup2Page.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace xamarinMeetup2
+{
+    public partial class xamarinMeetup2Page
+    {
+        public xamarinMeetup2Page()
+        {
+            InitializeComponent();
+
+            // get embedded resource into string first
+            var assembly = typeof(xamarinMeetup2Page).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("xamarinMeetup2.MOCK_DATA.json");
+            string source = "";
+            using (var reader = new StreamReader(stream))
+            {
+                source = reader.ReadToEnd();
+            }
+
+            // assign deserialized source to listview
+            list.ItemsSource = JsonConvert.DeserializeObject<List<ItemModel>>(source);
+
+            calculateButton.Clicked += Calculate;
+        }
+
+        void Calculate(object sender, EventArgs e)
+        {
+            mathPage.BindingContext = new MathModel();
+        }
+    }
+}
