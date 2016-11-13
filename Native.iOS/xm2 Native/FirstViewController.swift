803534c8dc150d16b0fbad5fd9475171e4be3603
Yury Nevalenny <nevalenny@gmail.com>
2016-11-13 23:54:34 +0300

    implement Native.iOS list

--- Native.iOS/xm2 Native/FirstViewController.swift
+++ Native.iOS/xm2 Native/FirstViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import SwiftyJSON
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
+    var cellId:String = "ItemCell"
+    var items: Array<JSON>? = nil
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let path = Bundle.main.path(forResource: "MOCK_DATA", ofType: "json")
+        {
+            do {
+                let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+                let jsonObj = JSON(data: data)
+                if jsonObj != JSON.null {
+                    items = jsonObj.arrayValue
+                } else {
+                    print("Could not get json from file, make sure that file contains valid json.")
+                }
+            } catch let error {
+                print(error.localizedDescription)
+            }
+            
+            tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+            tableView.delegate = self
+            tableView.dataSource = self
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items?.count ?? 0
+    }
 
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // cell selected code here
+    }
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let item = items?[indexPath.row]
+        let cell:UITableViewCell = tableView.dequeueReusableCell(withIdentifier: cellId)! as UITableViewCell
+        
+        let captionText = (item?["first_name"].stringValue ?? "") + " " + (item?["last_name"].stringValue ?? "")
+        
+        cell.textLabel?.text = captionText
+        
+        let pictureString = String((item?["picture"].stringValue ?? "").characters.dropFirst(22))
+        
+        let dataDecoded:Data = Data(base64Encoded: pictureString, options: NSData.Base64DecodingOptions(rawValue: 0))!
+        
+        cell.imageView?.image = UIImage(data: dataDecoded)!
+        return cell
+    }
 }
 
