d51c2f253a22fab26949987616becce872514b6e
Yury Nevalenny <nevalenny@gmail.com>
2016-11-14 02:10:36 +0300

    implement Native.iOS calculations

---
+++ Native.iOS/xm2 Native/MathModel.swift
@@ -0,0 +1,183 @@
+//
+//  MathModel.swift
+//  xm2 Native
+//
+//  Created by Yury Nevalenny on 14/11/2016.
+//  Copyright © 2016 Rahmobile. All rights reserved.
+//
+
+import Foundation
+
+class MathModel {
+    public var arithmeticPerformance: String = ""
+    public var collectionsPerformance: String = ""
+    public var stringsPerformance: String = ""
+    
+    public func calculate()
+    {
+        calcArithmetic ()
+        calcCollections ()
+        calcStrings ()
+    }
+    
+    func calcArithmetic ()
+    {
+        var arithmetic: Double = 0
+        
+        for _ in 0..<5
+        {
+            let start = DispatchTime.now()
+            
+            var pans : Int = 0
+            var primes : Int = 0
+            var foo : Double = 0
+            
+            for i in 0..<12345
+            {
+                if isPandigital(n: i) { pans+=1 }
+                
+                if isPrime(n: i) { primes+=1 }
+                
+                foo += calculateFoo(n: i)
+            }
+            
+            let end = DispatchTime.now()
+            let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
+            let timeInterval = Double(nanoTime) / 1_000_000
+            
+            arithmetic += timeInterval
+        }
+        
+        arithmetic /= 5
+        arithmeticPerformance = String(Int(arithmetic)) + " ms"
+    }
+    
+    func calcCollections ()
+    {
+        var collections: Double = 0
+        
+        for _ in 0..<5
+        {
+            let start = DispatchTime.now()
+            
+            
+            var list = Array<FooContainer>(); //let's don't specify initial capacity
+            for i in 0..<1000000
+            {
+                var f = FooContainer()
+                f._x=i
+                f._y=i
+                list.append(f)
+            }
+            
+            var filteredList = Array<FooContainer>()
+            for i in 0..<list.count
+            {
+                if list[i].Sum() % 2 == 0 {
+                    filteredList.append(list[i])
+                }
+            }
+            
+            let end = DispatchTime.now()
+            let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
+            let timeInterval = Double(nanoTime) / 1_000_000
+            
+            collections += timeInterval
+        }
+        
+        collections /= 5
+        collectionsPerformance = String(Int(collections)) + " ms"
+    }
+    
+    func calcStrings ()
+    {
+        var strings: Double = 0
+        
+        for _ in 0..<5
+        {
+            let start = DispatchTime.now()
+            
+            var result = ""
+            for i in 0..<100000
+            {
+                result.append(String(i))
+            }
+            
+            let index = result.index(result.startIndex, offsetBy: 1000)
+            result = result.substring(from: index)
+            result = result.replacingOccurrences(of: "10", with: "")
+            let containsSubstring = result.contains("123")
+            let parts = result.components(separatedBy: "2")
+            let length = parts.count
+            
+            let end = DispatchTime.now()
+            let nanoTime = end.uptimeNanoseconds - start.uptimeNanoseconds
+            let timeInterval = Double(nanoTime) / 1_000_000
+            
+            strings += timeInterval
+        }
+        
+        strings /= 5
+        stringsPerformance = String(Int(strings)) + " ms"
+    }
+    
+    func isPrime(n:Int) -> Bool
+    {
+        if n % 2 == 0 { return false }
+        
+        var i = 3
+        while i*i<=n {
+            i+=3
+            if n % i == 0 { return false }
+        }
+        
+        return true;
+    }
+    
+    func isPandigital(n: Int) -> Bool
+    {
+        var index = n
+        var count = 0
+        var digits = 0
+        var digit = 0
+        var bit = 0
+        
+        repeat
+        {
+            digit = index % 10;
+            if digit == 0 { return false }
+            bit = 1 << digit
+    
+            let check = digits | bit
+            if digits == check { return false }
+            digits = check
+    
+            count += 1
+            index /= 10
+        } while (n > 0)
+        
+        return (1 << count) - 1 == digits >> 1;
+    }
+    
+    func calculateFoo(n : Int) -> Double
+    {
+        var a = 1.0
+        for _ in 0..<n
+        {
+            a = a * 1.123456789101112 / Double(n)
+        }
+        
+        return a
+    }
+}
+
+struct FooContainer
+{
+    public var _x = 0
+    public var _y = 0
+    
+    public func Sum () -> Int
+    {
+        return _x + _y
+    }
+}
--- Native.iOS/xm2 Native/SecondViewController.swift
+++ Native.iOS/xm2 Native/SecondViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    @IBOutlet weak var arithmeticLabel: UILabel!
+    @IBOutlet weak var collectionsLabel: UILabel!
+    @IBOutlet weak var stringsLabel: UILabel!
+    @IBOutlet weak var calculateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func Calculate(_ sender: Any) {
+        DispatchQueue.global(qos: .background).async {
+            let mathModel = MathModel()
+            mathModel.calculate()
+            
+            DispatchQueue.main.async {
+                self.arithmeticLabel.text = "Arithmetic: "+mathModel.arithmeticPerformance
+                self.collectionsLabel.text = "Collections: "+mathModel.collectionsPerformance
+                self.stringsLabel.text = "Strings: "+mathModel.stringsPerformance
+            }
+        }
+    }
 }
 
